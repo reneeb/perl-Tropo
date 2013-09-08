506269f337ac184ee1a308815fb81c5f65ea0991
Renee Baecker <github@renee-baecker.de>
2013-09-08 10:13:34 -0700

    work on Tropo API

--- lib/Tropo.pm
+++ lib/Tropo.pm
@@ -36,6 +36,8 @@ for my $subname ( qw(call say ask on wait) ) {
         
         my $obj = $module->new( @params );
         $tropo->add_object( { $subname => $obj } );
+
+        return $tropo;
     };
 }
 
@@ -110,11 +112,9 @@ Creates this JSON output:
   {
       "tropo":[
           {
-              "call": [
-                  {
+              "call": {
                       "to":"+14155550100"
-                  }
-              ]
+              }
           },
           {
               "say": [
@@ -127,5 +127,70 @@ Creates this JSON output:
   }
 
 =head1 HOW THE TROPO API WORKS
+
+The Tropo server I<talks> with your web application via json sent with HTTP POST requests.
+
+When you'd like to initiate a call/text message, you have to start a session.
+
+        my $session = Tropo::RestAPI::Session->new(
+            url => 'https://tropo.developergarden.com/api/', # use developergarden.com api
+        );
+
+        my $data = $session->create(
+            token        => $token,
+            call_session => $id,
+        ) or print $session->err;
+
+When you create a session you can pass any parameter you want. The only mandatory parameter
+is I<token>. You'll find that token in your developergarden account in the application management.
+
+The Tropo server then requests the URI that you added in the application management. It is an
+HTTP POST request that contains a session id and some more
+
+=head1 COMMANDS
+
+This list show the commands currently implemented. This library is under heavy development,
+so that more commands will follow in the near future:
+
+=head2 ask
+
+=head2 call
+
+=head2 on
+
+=head2 say
+
+=head2 wait
+
+A detailed description of all commands and their attributes can be found at
+L<http://www.developergarden.com/fileadmin/microsites/ApiProject/Dokumente/Dokumentation/Api_Doc_5_0/telekom-tropo-2.1/html/method_summary.html|DeveloperGarden>.
+
+=head1 EXAMPLES
+
+All examples can be found in the I<examples> directory of this distribution. Those examples
+might have extra dependencies that you might have to install when you want to run the code.
+
+You also need an account e.g. for developergarden.com or tropo.com.
+
+=head2 Two factor authentication
+
+I<call_customer.psgi>
+
+You can find a small C<Mojolicious::Lite> application that calls a customer to tell him a
+code... On the start page a small form is shown where the customer sends his phone number.
+Then a new call is initiated and the Tropo provider calls the customer and tells him the
+secret.
+
+=head2 Handle incoming calls
+
+I<televote.psgi>
+
+You can publish a phonenumber that is connected to your application (e.g. in developergardens
+application management).
+
+=head1 ACKNOWLEDGEMENT
+
+I'd like to thank Richard from Telekoms Developergarden. He has done a lot of debugging during
+the #startuphack (Hackathon at "Lange Nacht der Startups" 2013).
 
 =cut
--- lib/Tropo/WebAPI/Ask.pm
+++ lib/Tropo/WebAPI/Ask.pm
@@ -15,8 +15,9 @@ Tropo::WebAPI::Base::register();
 
 our $VERSION = 0.01;
 
-has say           => (is => 'ro', isa => Str, required => 1);
+has text          => (is => 'ro', isa => Str, required => 1);
 has attempts      => (is => 'ro', isa => Int);
+has timeout       => (is => 'ro', isa => Int);
 has choices       => (is => 'ro', isa => Str);
 has voice         => (is => 'ro', isa => Str);
 has allow_signals => (is => 'ro', isa => ArrayRef[]);
@@ -24,12 +25,35 @@ has allow_signals => (is => 'ro', isa => ArrayRef[]);
 sub BUILDARGS {
    my ( $class, @args ) = @_;
  
-  unshift @args, "say" if @args % 2 == 1;
+  unshift @args, "text" if @args % 2;
 
-  my %param = @args;
-  $param{say} = $param{text} if $param{text};
- 
-  return \%param;
+  return { @args };
 }
 
 1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Tropo::WebAPI::Ask - "Ask" something with Tropo
+
+=head1 VERSION
+
+version 0.14
+
+=head1 AUTHOR
+
+Renee Baecker <module@renee-baecker.de>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is Copyright (c) 2013 by Renee Baecker.
+
+This is free software, licensed under:
+
+  The Artistic License 2.0 (GPL Compatible)
+
+=cut
